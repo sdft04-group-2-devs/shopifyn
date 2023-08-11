8f2b6975052b7f1cf9b353a050e8ceee135a7ecd
Owallah <mikesonowallah@gmail.com>
2023-08-11 17:03:02 +0300

    update on the backend

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,12 +10,22 @@ class ProductsController < ApplicationController
       products = Product.all
       render json: products, status: :ok
     end
+
+    def create
+      product = Product.create(product_params)
+      if product
+        render json: product, status: :created
+      else
+        render json: {error: 'Unprocessable entity'}, status: :unprocessable_entity
+      end
+
+    end
   
     def show
       product = Product.find_by(id: params[:id])
   
       if product
-        render json: product, except: [:created_at, :updated_at], status: :ok
+        render json: product, except: [:created_at, :updated_at], include: :reviews, status: :ok
       else
         render json: { error: 'Product not found' }, status: :not_found
       end
@@ -24,5 +34,9 @@ class ProductsController < ApplicationController
     private
     def set_product
         @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.permit(:name, :price, :description, :brand, :stock_quantity, :image_url_1, :image_url_2, :image_url_3)
     end
   end
