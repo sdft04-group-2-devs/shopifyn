5008fd78b79a7b9a60fa75aade10b1ca87188d2c
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-08-14 11:41:44 +0300

    feat:deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    
     resource '*',
-            headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head]
-	
-	resource '/orders', # for specific restrictions(optional)
-      headers:any,
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '/orders', # for specific restrictions(optional)
+      headers: :any,
       methods: [:post]
   end
 end
