20bd9e5321c5ffe757f577c7fb371598e61a8081
Owallah <mikesonowallah@gmail.com>
2023-08-19 16:05:28 +0300

    Updated the models and controllers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
-    # belongs_to :category
     has_many :order_items
     has_many :reviews
     has_many :users, through: :wishlists
-    belongs_to :cart_item
+    
 end
